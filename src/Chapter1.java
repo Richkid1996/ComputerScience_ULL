dbebea22ce129c82887e49715ff8b5ad00d1bb0c
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-01 02:18:42 -0600

    Created class for Chapter1 and add the first two questions of quiz1.7

---
+++ src/Chapter1.java
@@ -0,0 +1,22 @@
+/* This Class will be covering Chapter 1 which is the introduction to Programming were we began to cover the basics of
+Java and began to start programing
+ */
+public class Chapter1 {
+    public static void main(String[] args) {
+        System.out.println("---Running Chapter 1 Practice ---");
+
+        Question1(); //  Quiz 1.7 Question 1
+        Question2(); // 
+    }
+
+    //Quiz 1.7 Question 1:
+    /* Goal: Learn how to display messages to the console output.
+    Assignment: In the quiet village of Codeville, a tradition exist where newcomers announce their arrival at the
+    central square. Write a Java statement to print "New coder in Codeville, ready to explore!" to the screen.
+     */
+    public static void Question1() {
+        System.out.println("New coder in Codeville, ready to explore!");
+    }
+    //Quiz 1.7 Question 2:
+
+}
