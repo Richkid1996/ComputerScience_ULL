4485969e967723670a401e5c817edb50759de6f4
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-03 14:23:06 -0600

    Created class for Chapter1 and add the first two question Added notes about Chapter2 Added the first question of Quiz 2.5 were we use Double values to store prices of items Added Question 2 were we use myScanner Updated overall code by importing the Scanner and cleaning up question 2 layout Created new file Chapter4 and added question 1
    Updated Headers for Chapter2
    Updated the solution for Question 1 of chapter4

--- src/Chapter4.java
+++ src/Chapter4.java
@@ -3,9 +3,9 @@
 
 public class Chapter4 {
     public  static void main(String[] args) {
-       char c1 = 'A';
-       char c2 = 'B';
-        Question1();// Quiz 4.3 Question 1
+       char c1 = 'a';
+       char c2 = 'b';
+        Question1(c1,c2 );// Quiz 4.3 Question 1
     }
     /*Goal: Familiarize with the operations of characters.
     Assignment: You are developing your first messaging app. As part of an effort top make communications more secure,
@@ -14,7 +14,7 @@ public class Chapter4 {
     Assume two char variables c1 and c2 have been declared and assigned a value. Write a code snippet that prints out the
     character associated with the sum of the integer values of c1 and c2.
      */
-    public static void Question1() {
+    public static void Question1(char c1, char c2) {
 
         //We sum the values, then cast the total back to a (char)
         System.out.println((char)(c1+c2));
