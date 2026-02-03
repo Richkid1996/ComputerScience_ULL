637f306443b026c2278370111d4ade76662986db
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-03 00:00:43 -0600

    Created class for Chapter1 and add the first two question Added notes about Chapter2 Added the first question of Quiz 2.5 were we use Double values to store prices of items

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -1,5 +1,21 @@
 //This Class will be covering Chapter 2 were we will be learning about variables and how they may be changed on the program
 
 public class Chapter2 {
+    public static void main(String[] args) {
+        System.out.println("----Running Chapter 2 Practice----");
 
+        Question1(); // Quiz 2.5 Variables Question1
+    }
+
+    //Quiz 2.5 Question 1
+    /*Goal: Learn how to declare and initialize variables
+    Assignment: When preparing a recipe, you need to measure the volume of liquid ingredients in liters.
+    Declare two double variables, one named milk with a value of 0.75 and the other named water with a value of 1.25.
+     */
+    public static void Question1() {
+        double milk = 0.75;
+        double water = 1.25;
+        System.out.printf("Milk cost: " + milk + " Water cost: " + water);
+
+    }
 }
