f31ff5de3a55e9103fbff8a523ead68428d6cd30
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-03 12:35:48 -0600

    Created class for Chapter1 and add the first two question Added notes about Chapter2 Added the first question of Quiz 2.5 were we use Double values to store prices of items Added Question 2 were we use myScanner Updated overall code by importing the Scanner and cleaning up question 2 layout
    Updated Headers for Chapter2

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -1,4 +1,6 @@
-/*This Class will be covering Chapter 2 were we will be learning about variables and how they may be changed on the program*/
+//Elementary Programming Chapter 2
+/*This Class will be covering Chapter 2 were we will be learning about variables and how they may be changed on the program
+We will be covering how to use Scanners */
 
 import java.util.Scanner;
 public class Chapter2 {
