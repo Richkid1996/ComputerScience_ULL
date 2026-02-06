07c25e6d0f1b91b17847036b9803fefe9e391383
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-05 23:52:11 -0600

    Added Question2 description and assignment comments Added Question3 description and assignment comments Added Quiz 2.6 Question 1 notes and solution
    Added Quiz 2.6 Question 2 notes
    
    Added Chapter3 created notes and the first Question

---
+++ src/Chapter3.java
@@ -0,0 +1,20 @@
+/*In Chapter 3 we are covering Boolean Data Types, Values, and Expressions and if else statements.
+ */
+public class Chapter3 {
+    public static void main(String[] args) {
+        System.out.println("---Running Chapter 3 Practice----");
+        Question1(); // Quiz 3.2 Boolean Data Types, Values, and Expresssions
+    }
+    //Quiz 3.2 Question1
+    /*Goal: Declare a boolean variable and assign its value.
+    Assignment: In a racing game, the boost of the player's vehicle is controlled by a variable called boost. Declare
+    this boolean variable and initialize it to false.*/
+    // We use the 'boolean' keyword to declare a variable that is either true or false
+    // 'boost' is the name of the variable (the identifier)
+    // '=' is the assignment operator
+    // 'false' is the initial value
+    public static void Question1() {
+
+    }
+
+}
