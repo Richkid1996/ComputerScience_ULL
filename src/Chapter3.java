897f9a5b03f8cc7f8f167336eff973cc797fd626
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-08 22:12:40 -0600

    Added Question2 description and assignment comments Added Question3 description and assignment comments Added Quiz 2.6 Question 1 notes and solution
    Added Quiz 2.6 Question 2 notes
    
    Added Chapter3 created notes and the first Question Added Question 2 and added notes and the solutions Added Question 3 and added solutions and comments

--- src/Chapter3.java
+++ src/Chapter3.java
@@ -5,7 +5,8 @@ public class Chapter3 {
     public static void main(String[] args) {
         System.out.println("---Running Chapter 3 Practice----");
         Question1(); // Quiz 3.2 Using the Boolean variable
-        Questions2();// Quiz 3.2 Learning to compare values
+        Question2();// Quiz 3.2 Learning to compare values
+        Question3();//Quiz 3.2 Learn to compare Values
     }
     //Quiz 3.2 Question1
     /*Goal: Declare a boolean variable and assign its value.
@@ -24,10 +25,11 @@ public class Chapter3 {
     Assignment: In a smart home system, the humidity level is constantly monitored to adjust the dehumidifier's settings for optimal comfort.
     Given a double variable humidityLevel, write an expression that is true if and only if the humidity level is equal or greater than 70.0.
     Assign the expression to a result variable, which has already been declared. */
+
     //Solution Breakdown:
     // Since the variable 'result' is already declared, we just assign it.
     // We use the '>=' operator for "greater than or equal to."
-    public static void Questions2() {
+    public static void Question2() {
         // 1. Create the Scanner 'tool' to read from the keyboard (System.in)
         Scanner inputReader = new Scanner(System.in);// Receives the input for the humidifier
 
@@ -39,6 +41,21 @@ public class Chapter3 {
 
         // Optional: Print the result so you can see if it worked!
         System.out.println("The Humidity Level is:" + result);
+
+    }
+    /*Goal: Learn to compare values.
+    Assignment: To calculate whether an employee has earned overtime pay, the company software needs to compare the
+    employee's work hours for the week compared to their contracted hours.
+    Write an expression that evaluates as true if the variable accruedWeekHours is greater than contractHours and assign
+    it to the variable result. Assume that all variables have already been declared*/
+
+    //Solution Breakdown:
+
+    public static void Question3() {
+        int accuedWeekHours = 65;
+        int contractHours = 40;
+        boolean result = accuedWeekHours > contractHours;
+        System.out.println("Is this weeks hours greater than your contracted:? " + result);
 
     }
 
