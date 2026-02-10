e70eedef2de8fae39be22a6850bba51e31ae422f
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-09 23:23:32 -0600

    Added Question2 description and assignment comments Added Question3 description and assignment comments Added Quiz 2.6 Question 1 notes and solution
    Added Quiz 2.6 Question 2 notes
    
    Added Chapter3 created notes and the first Question Added Question 2 and added notes and the solutions Added Question 3 and added solutions and comments
    ADDED Question4 and notes with solutions
    ADDED multiple questions and solutions and also improved programs by adding input Scanners in origianl programs

--- src/Chapter3.java
+++ src/Chapter3.java
@@ -8,6 +8,10 @@ public class Chapter3 {
         Question2();// Quiz 3.2 Learning to compare values
         Question3();//Quiz 3.2 Learn to compare Values
         Question4();// Quiz 3.2 Learn to compare values
+        Question5();// Quiz 3.3 (Question 1)Learn to write If statements
+        Question6();// Quiz 3.3 (Question 2)Learn to write If statements
+        Question7();//Quiz 3.4 Learn to write if else statements
+        Question8();// Quiz 3.13 Understand how to use switch statements to handle multiple conditions.
     }
     //Quiz 3.2 Question1
     /*Goal: Declare a boolean variable and assign its value.
@@ -33,6 +37,7 @@ public class Chapter3 {
     public static void Question2() {
         // 1. Create the Scanner 'tool' to read from the keyboard (System.in)
         Scanner inputReader = new Scanner(System.in);// Receives the input for the humidifier
+        System.out.println("What is the percentage of Humidity? ");
 
         // 2. Tell the tool to grab the next decimal (double) the user types
         double humidityLevel = inputReader.nextDouble();
@@ -41,7 +46,7 @@ public class Chapter3 {
         boolean result = humidityLevel >= 70.0;
 
         // Optional: Print the result so you can see if it worked!
-        System.out.println("The Humidity Level is:" + result);
+        System.out.println("The Humidity Level is greater than 70 :" + result);
         System.out.println("");
 
     }
@@ -80,5 +85,67 @@ public class Chapter3 {
         System.out.println("Are the values (a)(b)(c) less than 100? " + result2);
         System.out.println("");
     }
+    //Quiz 3.3 Question1
+    /*Goal: Learn to write if statements.
+    Assignment: In a mobile gaming app, a message is displayed if the player beats the current high score. Given the
+    variables playerScore and highScore, write an if statement that prints New High Score! on the screen if the player
+    score is strictly greater than the current high score.
+     */
+    //Solution Breakdown:
+    //1.Create the highScore variable
+    //2.Create the Scanner that is called input were we get the players Score from the user
+    //3.Then we create a output statement asking the user what's there score
+    //4. Then we create a if statement testing whither if the players score is greater than the highScore
+    public static void Question5(){
+       int highScore = 865;// Original HighScore value
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your score? ");
+        int playerScore = input.nextInt();
+
+        if (playerScore > highScore) {
+            System.out.println("We have a New HighScore!!!");
+        }
+    }
+    //Quiz 3.3 Question2
+    /*Goal: Learn to write if statements.
+    Assignment: In a logistics management system for a food delivery service, the delivery fee is adjusted based on how
+    many deliveries are scheduled in the same area, in the same time range: if more deliveries can be completed in a single trip,
+    the fee per customer is reduced.
+    Given the variables int numberOfDeliveries and int deliveryDiscount already declared and assigned, reassign deliveryDiscount
+    to the value 15 if the number of deliveries exceeds 50.
+     */
+    //Solution:
+    //1. Created a new scanner to get the amount of deliveries for today
+    //2. assigned the input to the int variable numberOfDeliveries
+    //3.Created the int variable that had the original discount percentage
+    //4. created the if statement to change the discount percentage if the deliveries are over 50
+    public static void Question6() {
+        Scanner delivery = new Scanner(System.in);
+        System.out.println("How many Deliveries do we have today? ");
+        int numberOfDeliveries = delivery.nextInt();
+         int deliveryDiscount = 20;;
+        if (numberOfDeliveries > 50) {
+            deliveryDiscount = 15;
+            System.out.println("What is the delivery discount? " + deliveryDiscount);
+            System.out.println("");
+        }
+
+    }
+    //Quiz 3.4 Question1
+    public static void Question7() {
+
+    }
+    //Quiz 3.13 Question 1
+    /* Goal: Understand how to use switch statements to handle multiple conditions.
+    Assignment: Write a full program with class name Calculator to solve a calculator problem that prompts the user for
+    2 (double) numbers and an operator (char) and outputs the result.
+    */
+    //Solution:
+
+    public static void Question8() {
+
+    }
+
 
 }
