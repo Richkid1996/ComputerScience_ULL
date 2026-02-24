4ef1d8b8529b728ec8a22d89673119ea5788dd96
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-24 17:31:11 -0600

    Added Question2 description and assignment comments Added Question3 description and assignment comments Added Quiz 2.6 Question 1 notes and solution
    Added Quiz 2.6 Question 2 notes
    
    Added Chapter3 created notes and the first Question Added Question 2 and added notes and the solutions Added Question 3 and added solutions and comments
    ADDED Question4 and notes with solutions
    ADDED multiple questions and solutions and also improved programs by adding input Scanners in origianl programs
    Added question1 for Quiz 3.4
    
    Added more programs were we are using if and else if statements along with notation
    Completing Chapter3 programing practice
    
    Created chapter 5 and added notes and program
    
    Added chapter 6 and 7

--- src/Chapter6.java
+++ src/Chapter6.java
@@ -2,9 +2,45 @@ public class Chapter6 {
     public static public static void main(String[] args) {
         System.out.println("----Running Chapter 7 Practice------");
         Question1();//Quiz 6.2 Learn to write static methods
+        Question2();//Quiz 6.2 Learn to write static methods
+        Question3();//Quiz 6.2 Learn to write static methods
+        Question4();//Quiz 6.3 Learn to call methods
     }
     //Quiz 6.2 Question 1
+    /*Goal: Learn to write static methods.
+
+    Assignment: Imagine you are a mathematician, and your goal is to make your calculations easier by placing common operations
+    into reusable methods. In this instance, you want to define a method that automatically triples a number for you.
+    Define a public static method called triple, which receives a single int as a parameter. The method should multiply
+    the provided argument by 3 and return the result as an int.
+     */
     public static void Question1(){
+
+    }
+    //Quiz 6.2 Question 2
+    /*Goal: Learn to write static methods.
+
+    Assignment: Imagine you are a mathematician whose goal is to make your calculations easier by placing common operations
+    into reusable methods. In this instance, you want to define a method that automatically halves a number for you.
+    Define a public static method called half, which receives a single int as a parameter. The function should divide the
+    provided argument by 2 and return the result as a double.
+     */
+    public static void Question2() {
+
+    }
+    //Quiz 6.2 Question 3
+    /*Goal: Learn to write static methods.
+
+    Assignment: Imagine you are a developer creating an application for an online supermarket called "Green Grocers".
+    You are tasked with creating a personalized experience for every customer, beginning with a personalized welcome message.
+    Define a public static method called welcomeMessage that receives the name of the current user as a String parameter.
+    The method should then print the welcome message "Welcome to Green Grocers, [name]!".
+     */
+    public static void Question3() {
+
+    }
+    //Quiz 6.3 Question 1
+    public static void Question4() {
 
     }
 }
