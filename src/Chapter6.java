84802889ff20962245aeb44b09566a2e6cced514
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-02-28 00:00:06 -0600

    Added Question2 description and assignment comments Added Question3 description and assignment comments Added Quiz 2.6 Question 1 notes and solution
    Added Quiz 2.6 Question 2 notes
    
    Added Chapter3 created notes and the first Question Added Question 2 and added notes and the solutions Added Question 3 and added solutions and comments
    ADDED Question4 and notes with solutions
    ADDED multiple questions and solutions and also improved programs by adding input Scanners in origianl programs
    Added question1 for Quiz 3.4
    
    Added more programs were we are using if and else if statements along with notation
    Completing Chapter3 programing practice
    
    Created chapter 5 and added notes and program
    
    Added chapter 6 and 7

--- src/Chapter6.java
+++ src/Chapter6.java
@@ -4,7 +4,11 @@ public class Chapter6 {
         Question1();//Quiz 6.2 Learn to write static methods
         Question2();//Quiz 6.2 Learn to write static methods
         Question3();//Quiz 6.2 Learn to write static methods
-        Question4();//Quiz 6.3 Learn to call methods
+        Question4();//Quiz 6.3 Learn to call methods Question 1
+        Question5();//Quiz 6.3 Learn to call methods Question 2
+        Question6();//Quiz 6.3 Learn to call methods Question 3
+        Question7();//Quiz 6.3 Learn to call methods Question 4
+        Question8();//Quiz 6.4 Learn to write static void-returning methods
     }
     //Quiz 6.2 Question 1
     /*Goal: Learn to write static methods.
@@ -40,7 +44,49 @@ public class Chapter6 {
 
     }
     //Quiz 6.3 Question 1
+    /*Goal: Learn to call methods.
+
+    Assignment: Imagine you are a detective. You are using a Java program that helps you to keep track of known criminals.
+    A function called isCriminal has already been defined that takes the full name of the criminal and returns true or false if that person has a criminal record.
+    To test that this method works as expected, call the method isCriminal and provide the name "Jack the Ripper" as an argument.
+    Save the result to a variable called result, which has already been declared.
+     */
     public static void Question4() {
+        result = isCriminal("Jack the Ripper ")
+
+    }
+    //Quiz 6.3 Question 2
+    /*Goal: Learn to call methods.
+
+    Assignment: Imagine you are a video game developer. You are working on a PvP Battle Royal game. Your specific focus is on
+    the character "Jy Normus", a powerful barbarian that uses a club. Each character has their own special ability that can
+    only be used when the power is fully charged. Jy Normus's special ability is called "Club Smash".
+    You have created a method called clubSmash that executes this special ability. The method takes a single bool input
+    to determine whether the character's power is fully charged and doesn't return anything. To test that it works as expected, call the method clubSmash and provide true as an input argument.
+     */
+    public static void Question5() {
+
+    }
+    //Quiz 6.3 Question 3
+    /*Goal: Learn to call methods.
+
+    Assignment: Imagine that you are a hotel manager. You are in charge of tracking how many rooms are occupied at any given moment.
+    To make your life easier, you have defined a method getOccupancy that automatically checks the number of currently occupied rooms. The method doesn’t take any input arguments and returns an int.
+    Call the method getOccupancy and save the result to an already declared variable called result.
+     */
+    public static void Question6(){
+
+    }
+    //Quiz 6.3 Question 4
+    /*Goal: Learn to call methods.
+
+    Assignment: Imagine that you are the creator of a new toy called “The Orb of Wisdom” that, when asked a question and
+    then shaken, will show one of several random answers.
+    You have written a function called conjureWisdom that randomly selects one option from an array of strings and prints
+    it to the console. The method does not take any arguments and does not return anything. Call the conjureWisdom method.
+
+     */
+    public static void Question7(){
 
     }
 }
