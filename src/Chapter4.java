5b6682936ef0663353e0f06f4a3918f1a4b7d91d
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-03-02 23:39:01 -0600

    Updating and Reviewing Chapter4 quiz's

--- src/Chapter4.java
+++ src/Chapter4.java
@@ -1,14 +1,16 @@
 //Chapter 4:Mathmatical Functions, Characters, and strings
 //Quiz 4.3.4:Comparing and Testing Characters
-
+import java.util.Scanner;
 public class Chapter4 {
     public  static void main(String[] args) {
        char c1 = 'a';
        char c2 = 'b';
-        Question1(c1,c2 );// Quiz 4.3 Question 1 Using (char) for our variables
-        Question2();// Quiz 4.3 Question 2 using Character variable and a static method (.isLowerCase())
-        Question3();// quiz 4.3 Question3 manipulating characters
+        Question1(c1,c2 );// Quiz 4.3 Familiarize with the operations of characters.
+        Question2();// Quiz 4.3 Learn to use methods to validate characters.
+        Question3();// Quiz 4.3 Learn to write programs manipulating characters.
+        Question4();//Quiz 4.4 Learn to assign string literals.
     }
+    //Quiz 4.3 Question1
     /*Goal: Familiarize with the operations of characters.
     Assignment: You are developing your first messaging app. As part of an effort top make communications more secure,
     you decide to encrypt your messages using a string of characters of the same length, that both the sender and receiver
@@ -22,6 +24,7 @@ public class Chapter4 {
         System.out.println((char)(c1+c2));
         System.out.println("");//Creates Space between the output
     }
+    //Quiz 4.3 Question2
     /* Goal: Learn to use methods to validate characters.
     Assignment: As part of the login page of your app, you need to validate that each character of a user's username is valid.
     Given a char variable character, write a statement that assigns to the already declared boolean variable
@@ -35,6 +38,7 @@ public class Chapter4 {
         //Prints the final validation result to the console for the user to see
         System.out.println("(Is '" + character + "' a valid lowercase letter? " + isValid);
     }
+    //Quiz 4.3 Question3
     /*Goal: Learn to write programs manipulating characters.
     Assignment: You are currently working with the Unicode and ASCII encodings. Unfortunately, receiving numbers is not
     easily understandable for a human, and you need to quickly know what character is associated with a number and which
@@ -44,6 +48,22 @@ public class Chapter4 {
     Note: For ASCII, consider only the original 8-bit ASCII table, not the extended version.
      */
     public static void Question3() {
+    Scanner input = new Scanner(System.in);
+
+    if (input.hasNextInt()) {
+        int num = input.nextInt();
+        char character = (char) num;
+    }
+    }
+    //Quiz 4.4 Question1
+    /*Goal: Learn to assign string literals.
+
+    Assignment: In a project tracking system, each task has an author and a name. To make the tasks easily identifiable,
+    you decide to combine the two and use it as the key for your tasks.
+    Assume the String variables title and author have been declared and assigned a value. Write a code snippet that
+    declares the String variable key and assigns it the concatenation of the lower-case versions of title and author.
+     */
+    public static void Question4(){
 
     }
 }
