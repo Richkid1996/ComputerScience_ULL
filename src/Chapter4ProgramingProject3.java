94324738478c62c3e84f57050a47cd0bf91a977f
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-03-03 21:21:16 -0600

    Updating and Reviewing Chapter4 quiz's Added chapter 4 programing project 3

--- src/Chapter4ProgramingProject3.java
+++ src/Chapter4ProgramingProject3.java
@@ -1,2 +1,47 @@
+import java.util.Scanner;
+/*(Vowel or consonant?)
+
+Write a program that prompts the user to enter a letter and checks whether the letter is a vowel or consonant.
+For a non letter input, display "invalid input".
+
+Sample Run 1
+
+Enter a letter: B
+B is a consonant
+Sample Run 2
+
+Enter a letter: a
+a is a vowel
+Sample Run 3
+
+Enter a letter: #
+# is an invalid input
+ */
 public class Chapter4ProgramingProject3 {
+    public static void main(String[] args){
+        Scanner letterInput = new Scanner(System.in);
+        System.out.println("Please give me a letter: ");
+        String letter = letterInput.nextLine();
+
+
+        if (letter.length() !=1 ) {
+            System.out.println("invalid input");
+            return;
+        }
+        char character = letter.charAt(0);
+
+        if (!Character.isLetter(character)) {
+            System.out.println("# invalid input");
+        }
+        else {
+            char lowerCharacter = Character.toLowerCase(character);
+
+            if (lowerCharacter == 'a' || lowerCharacter == 'e' || lowerCharacter == 'i' || lowerCharacter == 'o' || lowerCharacter == 'u') {
+                System.out.println(letter + " is a vowel");
+            }else {
+                System.out.println(letter + " is a consonant");
+            }
+        }
+
+    }
 }
