08727bea38b9540c7f17b51a3b27fa8b38efda77
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2026-03-10 23:06:13 -0500

    Created and began editing chapter9 for learning how to declare a class

---
+++ src/Chapter9.java
@@ -0,0 +1,24 @@
+public class Chapter9 {
+    public static void main(String[] args ) {
+        System.out.println("------Running Chapter 9 practice----");
+        Question1();
+    }
+    //Quiz 9.1 Question 1
+    /*Goal: Learn to declare classes.
+
+    Assignment: In an application tracking the growth of plants in a garden, you need to start by establishing a
+    foundational class for various types of plants without adding any specific properties or behaviors yet.
+    Write the definition of a public class named Plant. This class should not include constructors, methods, or
+    instance variables at this stage, serving as a basic building block for future development.
+     */
+    /*Solution: public class Plant {
+    }
+
+    //Quiz 9.1 Question 2
+    /*
+
+     */
+
+     */
+    }
+}
